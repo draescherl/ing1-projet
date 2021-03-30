02018d45592a725bb2aa9a4e82a382c5e0187a7a
Lucas DRAESCHER <draescherl@eisti.eu>
2021-03-30 22:25:31 +0200

    Started analizing project subject

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -4,4 +4,14 @@ import Vuetify from 'vuetify/lib/framework';
 Vue.use(Vuetify);
 
 export default new Vuetify({
+    theme: {
+        themes: {
+            light: {
+                primary: '#005cd8',
+                secondary: '#93c90e',
+                accent: '#eaaa00',
+                error: '#ef426f',
+            },
+        },
+    }
 });
