0fa6d69605b66fe73745595c2745bd70f3519c93
Lucas DRAESCHER <draescherl@eisti.eu>
2021-03-31 11:23:34 +0200

    Defined colours

--- src/plugins/vuetify.js
+++ src/plugins/vuetify.js
@@ -7,10 +7,21 @@ export default new Vuetify({
     theme: {
         themes: {
             light: {
-                primary: '#005cd8',
-                secondary: '#93c90e',
-                accent: '#eaaa00',
-                error: '#ef426f',
+                primary: '#005cd8',     // blue
+                secondary: '#93c90e',   // green
+                accent: '#994878',      // purple
+                error: '#ef426f',       // pinkish
+                info: '#eaaa00',        // yellow
+                success: "#48a23f",     // green
+                warning: "#e63b11",     // orange
+                vert_linky: '#cce821',
+                white: '#ffffff',
+                black: '#575757',
+                aqua: '#0032a9',
+                sky: '#00a3e0',
+                brown: '#a39382',
+                blue_grey: '#688197',
+                grey: '#98a4ae'
             },
         },
     }
