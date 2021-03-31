b5a4551a10bfe3d75b1b281f24d2fcc5585c48fb
Lucas DRAESCHER <draescherl@eisti.eu>
2021-03-31 21:25:57 +0200

    Save

--- src/components/navbar.vue
+++ src/components/navbar.vue
@@ -1,21 +1,50 @@
 <template>
-  <v-app-bar
-    color="primary"
-    app
-  >
-  
-    <v-app-bar-nav-icon></v-app-bar-nav-icon>
-    <v-toolbar-title>Page title</v-toolbar-title>
-  
-  </v-app-bar>
+  <div>
+    <v-app-bar color="primary" app>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="secondary"
+        >
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-plus-box</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Saisir un article</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-chart-pie</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Voir les résultats</v-list-item-title>
+          </v-list-item>
+
+          <v-list-item>
+            <v-list-item-icon>
+              <v-icon>mdi-cog-outline</v-icon>
+            </v-list-item-icon>
+            <v-list-item-title>Page d'administration</v-list-item-title>
+          </v-list-item>
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
 </template>
 
 <script>
-  export default {
-    name: 'app-navbar',
+export default {
+  name: "app-navbar",
 
-    data: () => ({
-
-    })
-  }
+  data: () => ({
+    drawer: false,
+    group: null,
+  }),
+};
 </script>
