c37ab75edc5e825209ec0558b778a977aafc0a6e
Lucas DRAESCHER <draescherl@eisti.eu>
2021-03-31 11:23:34 +0200

    Defined colours

---
+++ src/components/navbar.vue
@@ -0,0 +1,21 @@
+<template>
+  <v-app-bar
+    color="primary"
+    app
+  >
+  
+    <v-app-bar-nav-icon></v-app-bar-nav-icon>
+    <v-toolbar-title>Page title</v-toolbar-title>
+  
+  </v-app-bar>
+</template>
+
+<script>
+  export default {
+    name: 'app-navbar',
+
+    data: () => ({
+
+    })
+  }
+</script>
