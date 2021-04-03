ba1b5bdb35f434f59dea63f41dfc131cd9749a41
Lucas DRAESCHER <draescherl@eisti.eu>
2021-04-04 00:11:47 +0200

    Links are now in an array

---
+++ src/components/Navbar.vue
@@ -0,0 +1,62 @@
+<template>
+  <div>
+    <v-app-bar color="primary" app>
+      <v-app-bar-nav-icon @click="drawer = true"></v-app-bar-nav-icon>
+
+      <v-toolbar-title>Title</v-toolbar-title>
+    </v-app-bar>
+
+    <v-navigation-drawer app v-model="drawer" absolute temporary>
+      <v-list nav dense>
+        <v-list-item-group
+          v-model="group"
+          active-class="secondary"
+        >
+
+          <NavbarItem 
+            v-for="item in links"
+            :icon="item.icon"
+            :text="item.text"
+            :key="item.text"
+          />
+
+        </v-list-item-group>
+      </v-list>
+    </v-navigation-drawer>
+  </div>
+</template>
+
+<script>
+import NavbarItem from './NavbarItem';
+
+export default {
+  name: "Navbar",
+  components: { 
+    NavbarItem 
+  },
+
+  data: function() {
+    return {
+      drawer: false,
+      group: null,
+      links: [
+        {
+          icon: "mdi-plus-box",
+          text: "Saisir un article",
+          link: ""
+        },
+        {
+          icon: "mdi-chart-pie",
+          text: "Voir les résultats",
+          link: ""
+        },
+        {
+          icon: "mdi-cog-outline",
+          text: "Page d'administration",
+          link: ""
+        }
+      ]
+    }
+  }
+};
+</script>
---
+++ src/components/NavbarItem.vue
@@ -0,0 +1,15 @@
+<template>
+  <v-list-item>
+    <v-list-item-icon>
+      <v-icon>{{ icon }}</v-icon>
+    </v-list-item-icon>
+    <v-list-item-title>{{ text }}</v-list-item-title>
+  </v-list-item>
+</template>
+
+<script>
+export default {
+  name: "NavbarItem",
+  props: ["icon", "text"],
+};
+</script>
