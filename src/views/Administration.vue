0bcd40f84bc78233d073ef5509d50460bd830185
Lucas DRAESCHER <draescherl@eisti.eu>
2021-04-04 12:08:35 +0200

    Links now work

---
+++ src/views/Administration.vue
@@ -0,0 +1,14 @@
+<template>
+  <div class="administration">
+    <h1>administration works</h1>
+  </div>
+</template>
+
+<script>
+export default {
+  name: 'Administration',
+  components: {
+    
+  }
+}
+</script>
