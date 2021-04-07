485f622b134409d6b3a4f8c6135e5a73261a6e44
Lucas DRAESCHER <draescherl@eisti.eu>
2021-04-07 19:25:40 +0200

    First test query to DB from Express

--- server/routes/api/types.js
+++ server/routes/api/types.js
@@ -3,6 +3,12 @@ const db = require('../../connection');
 
 const router = express.Router();
 
+db.query('SELECT * FROM `types`', (err, res) => {
+  if (err)
+    console.log(err);
+  else
+    console.log(res);
+});
 
 // Get
 router.get('/', (req, res) => {
