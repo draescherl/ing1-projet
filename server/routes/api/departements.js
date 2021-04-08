f2e109ffb335335a572baa1aa019ddda3ed737af
Lucas DRAESCHER <draescherl@eisti.eu>
2021-04-08 20:53:04 +0200

    Complete CRUD for departements and themes

--- server/routes/api/departements.js
+++ server/routes/api/departements.js
@@ -3,12 +3,46 @@ const db = require('../../connection');
 
 const router = express.Router();
 
+// Create
+router.post('/', (req, res) => {
+  db.query('INSERT INTO departements SET ?', [req.body], (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.status(201).send();
+    }
+  });
+});
+
+// Read
 router.get('/', (req, res) => {
-  db.query('SELECT * FROM `departements`', (error, result) => {
+  db.query('SELECT * FROM departements', (error, result) => {
     if (error) {
       console.log(error);
     } else {
       res.send(result);
+    }
+  });
+});
+
+// Update
+router.put('/:id', (req, res) => {
+  db.query('UPDATE departements SET ? WHERE id = ?', [req.body, req.params.id], (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.status(200).send();
+    }
+  });
+});
+
+// Delete
+router.delete('/:id', (req, res) => {
+  db.query('DELETE FROM departements WHERE id = ?', [req.params.id], (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.status(200).send();
     }
   });
 });
--- server/routes/api/themes.js
+++ server/routes/api/themes.js
@@ -3,12 +3,46 @@ const db = require('../../connection');
 
 const router = express.Router();
 
+// Create
+router.post('/', (req, res) => {
+  db.query('INSERT INTO themes SET ?', [req.body], (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.status(201).send();
+    }
+  });
+});
+
+// Read
 router.get('/', (req, res) => {
-  db.query('SELECT * FROM `themes`', (error, result) => {
+  db.query('SELECT * FROM themes', (error, result) => {
     if (error) {
       console.log(error);
     } else {
       res.send(result);
+    }
+  });
+});
+
+// Update
+router.put('/:id', (req, res) => {
+  db.query('UPDATE themes SET ? WHERE id = ?', [req.body, req.params.id], (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.status(200).send();
+    }
+  });
+});
+
+// Delete
+router.delete('/:id', (req, res) => {
+  db.query('DELETE FROM themes WHERE id = ?', [req.params.id], (error, result) => {
+    if (error) {
+      console.log(error);
+    } else {
+      res.status(200).send();
     }
   });
 });
