eb43cdeb04964092fd497c56c3974b4021a861fe
Lucas DRAESCHER <draescherl@eisti.eu>
2021-04-10 00:22:25 +0200

    Sources now adapt to type and departement

--- client/src/services/DepartementService.js
+++ client/src/services/DepartementService.js
@@ -11,7 +11,7 @@ class DepartementService {
         const data = res.data;
 
         // Here we loop through the response object and extract the data 
-        Object.keys(data).forEach(index => to_return.push(data[index].num));
+        Object.keys(data).forEach(index => to_return.push(data[index]));
 
         resolve(to_return);
       }).catch(err => reject(err));
--- client/src/views/Saisie.vue
+++ client/src/views/Saisie.vue
@@ -8,15 +8,20 @@
     <v-form>
       <div v-if="selected == 0">
         <Retombee
-          :type="types"
-          :departement="departements"
+          :type="types_name"
+          :departement="departements_num"
           :sources="sources"
-          :theme="themes"
+          :theme="themes_name"
+          @change="updateSources"
         />
       </div>
 
       <div v-if="selected == 1">
-        <Communique :type="types" :departement="departements" :theme="themes" />
+        <Communique
+          :type="types_name"
+          :departement="departements_num"
+          :theme="themes_name"
+        />
       </div>
     </v-form>
   </div>
@@ -40,23 +45,60 @@ export default {
 
   data: () => ({
     selected: 0,
-    types: [],
-    departements: [],
+
+    types_name: [],
+    types_id: [],
+
+    departements_num: [],
+    departements_id: [],
+
+    themes_name: [],
+    themes_id: [],
+
     sources: [],
-    themes: [],
   }),
 
   methods: {
     select(tab) {
       this.selected = tab;
     },
+    async updateSources(value) {
+      const allDefined = value.every((e) => e != undefined);
+      if (allDefined) {
+        const type_index = this.types_name.indexOf(value[0]);
+        const dep_index = this.departements_num.indexOf(value[1]);
+
+        const type_id = this.departements_id[type_index];
+        const dep_id = this.departements_id[dep_index];
+
+        this.sources = await SourceService.getAll(type_id, dep_id);
+      }
+    },
   },
 
   async created() {
-    this.departements = await DepartementService.getAll();
-    this.sources = await SourceService.getAll(1, 1);
-    this.themes = await ThemeService.getAll();
-    this.types = await TypeService.getAll();
+    const departements = await DepartementService.getAll();
+    const themes = await ThemeService.getAll();
+    const types = await TypeService.getAll();
+
+    /** --------------------
+     * Separate id from data
+     * --------------------- */
+
+    departements.forEach((e) => {
+      this.departements_num.push(e.num);
+      this.departements_id.push(e.id);
+    });
+
+    types.forEach((e) => {
+      this.types_name.push(e.name);
+      this.types_id.push(e.id);
+    });
+
+    themes.forEach((e) => {
+      this.themes_name.push(e.name);
+      this.themes_id.push(e.id);
+    });
   },
 };
 </script>
